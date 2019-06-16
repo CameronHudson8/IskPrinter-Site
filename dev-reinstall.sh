2073d231a8ce303d3d6f8235e4da3b5e5c99ebd0
Cameron Hudson <35085463+CameronHudson8@users.noreply.github.com>
2019-06-16 14:51:05 -0700

    Fixed dev-reinstall.sh

--- dev-reinstall.sh
+++ dev-reinstall.sh
@@ -6,29 +6,32 @@ set -x
 # Remove installed dependencies
 rm -rf \
     ./backend/vendor \
-    ./backend/composer.lock
+    ./backend/composer.lock \
+    ./backend/lib
 
 # Download the ESI client, extract it, and install its dependencies
+mkdir ./backend/lib
 curl \
         -X POST "http://generator.swagger.io/api/gen/clients/php" \
         -H "Content-Type: application/json" \
         -d '{ "swaggerUrl": "https://esi.evetech.net/latest/swagger.json"}' \
     | jq -r '.link' \
-    | xargs curl -o backend/client-generated.zip
-mkdir backend/vendor
-unzip backend/client-generated.zip -d backend/vendor
-rm backend/client-generated.zip
-composer install -d "backend/vendor/php-client/SwaggerClient-php"
+    | xargs curl -o backend/lib/client-generated.zip
+unzip backend/lib/client-generated.zip  -d backend/lib
+rm backend/lib/client-generated.zip
 
-# Install the main app dependencies
+# # Install dependencies
+composer update -d "backend/lib/php-client/SwaggerClient-php"
+composer install -d "backend/lib/php-client/SwaggerClient-php"
+composer update -d "backend"
 composer install -d "backend"
 
-# Generate a secret key for Laravel
+# # Generate a secret key for Laravel
 php backend/artisan key:generate
 
-# # Download a fresh SDE
-# rm -rf /databases/eve-sde/*
-# curl https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2 | tar xjv -C /databases/eve-sde --strip=1
+# Download a fresh SDE
+rm -rf /databases/eve-sde/*
+curl https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2 | tar xjv -C /databases/eve-sde --strip=1
 
 ## Reinstall frontend stuff
 rm -rf \
