1606eb27b20ec6214f14b3c739caa6c7e0ee51e1
Cameron Hudson <CameronHudson8@gmail.com>
2020-09-20 17:16:30 -0700

    feat: add cors headers to backend

--- backend/express/src/app.ts
+++ backend/express/src/app.ts
@@ -8,6 +8,11 @@ let app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.use('/', indexRoutes);
 
