e6b8b48bf724472741b8caa7469293a227bac3df
Cameron Hudson <CameronHudson8@gmail.com>
2020-09-25 14:23:57 -0700

    feat: backend endpoint to get eve login url

--- backend/express/src/routes/api.ts
+++ backend/express/src/routes/api.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
 
+import loginUrlRoutes from 'src/routes/api/login-url';
 import tokenRoutes from 'src/routes/api/tokens';
 
 const router = express.Router();
 
+router.use('/login-url', loginUrlRoutes);
 router.use('/tokens', tokenRoutes);
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
