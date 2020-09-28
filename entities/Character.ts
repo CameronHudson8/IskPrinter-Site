d165ca36669590fa120bb5de01c0abad0b0188d1
Cameron Hudson <CameronHudson8@gmail.com>
2020-09-28 12:51:34 -0700

    feat: add character entity

---
+++ entities/Character.ts
@@ -0,0 +1,10 @@
+export interface Character {
+    CharacterID: number;
+    CharacterName: string;
+    ExpiresOn: Date;
+    Scopes: string;
+    TokenType: string;
+    CharacterOwnerHash: string;
+    IntellectualProperty: string;
+}
+  
