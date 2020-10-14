a2db6e0f4ce0639c4978d9916c909c813c76eba2
Cameron Hudson <CameronHudson8@gmail.com>
2020-10-13 17:52:26 -0700

    feat: throttle requests

--- frontend/angular/src/app/modules/request-throttler/request-throttler.ts
+++ frontend/angular/src/app/modules/request-throttler/request-throttler.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { observable, Observable, of, Subscription } from 'rxjs';
+import { Subscriber } from 'rxjs/internal/Subscriber';
+import { catchError, concatMap, tap } from 'rxjs/operators';
+
+import { RequestThrottlerService } from 'src/app/services/request-throttler/request-throttler.service';
 
 @NgModule({
   declarations: [],
@@ -11,10 +15,74 @@ import { Observable } from 'rxjs';
 })
 export class RequestThrottler implements HttpInterceptor {
 
-  private readonly THROTTLE_LIMIT = 8;
+  private static readonly THROTTLE_LIMIT = 1;
+
+  private requestsInProgress: number = 0;
+  private requestQueue: [
+    Subscriber<HttpEvent<any>>,
+    HttpRequest<any>,
+    HttpHandler,
+    Subscription
+  ][] = [];
+
+  constructor(
+    private requestThrottler: RequestThrottlerService,
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req);
+
+    console.log(`requestsInProgress = ${this.requestsInProgress}`);
+
+    return new Observable<HttpEvent<any>>((observer) => {
+
+      if (this.requestsInProgress < RequestThrottler.THROTTLE_LIMIT) {
+        this.requestsInProgress += 1;
+        const subscription = next.handle(req).subscribe(
+          (response) => observer.next(response),
+          (err) => observer.error(err),
+          () => {
+            observer.complete();
+            this.requestsInProgress -= 1;
+
+            this.tryToProcessNextRequest();
+
+          }
+        );
+        return () => subscription.unsubscribe();
+      }
+
+      const futureSubscription = new Subscription();
+      this.requestQueue.push([ observer, req, next, futureSubscription ]);
+
+      return () => {
+        console.log('unsubscribing...');
+        futureSubscription.unsubscribe();
+      }
+    });
+  }
+
+  tryToProcessNextRequest()  {
+    if (this.requestQueue.length <= 0) {
+      return;
+    }
+
+    const [ observer, req, next, futureSubscription ] = this.requestQueue.shift();
+
+    if (futureSubscription.closed) {
+      return;
+    }
+    this.requestsInProgress += 1;
+    const subscription = next.handle(req).subscribe(
+      (response) => observer.next(response),
+      (err) => observer.error(err),
+      () => {
+        observer.complete();
+        this.requestsInProgress -= 1;
+        this.tryToProcessNextRequest();
+      }
+    );
+    futureSubscription.unsubscribe = subscription.unsubscribe;
+
   }
 
 }
